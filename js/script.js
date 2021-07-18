9935e6e9512f319dcb1db4c09d97f54161581099
Rodrigo Lisboa <rl.lisboas@gmail.com>
2021-07-18 12:50:39 -0300

    Landing Page challenger

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let telefone = document.getElementById('telefone').value;
+    let data = {
+        nome,
+        email,
+        telefone,
+    }
+    let convertData = JSON.stringify(data);
+
+    localStorage.setItem('lead', convertData)
+
+    let content = document.getElementById('content')
+
+    let carregando = `<p>Carregando...<p>`
+
+    let pronto = `<p>Pronto!<p>`
+
+    content.innerHTML = carregando
+
+    setTimeout(() => {
+        content.innerHTML = pronto
+    }, 1000)
+    
+})
